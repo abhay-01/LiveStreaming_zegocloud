7792927def7fd79b2a6564f7e9d0e2053d141f0d
abhay-01 <pratapsinghabhay999@gmail.com>
2024-11-17 19:33:37 +0530

    fix:components removal

--- src/components/ViewerView.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { ZegoTextureView } from 'zego-express-engine-reactnative';
-
-export default function ViewerView() {
-  return <ZegoTextureView style={{ flex: 1 }} />;
-}
